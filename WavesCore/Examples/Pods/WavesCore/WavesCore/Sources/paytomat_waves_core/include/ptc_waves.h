ae5c3db1602c1ba8a804ec1e94939a6d335c9fe0
Alex Melnichuk <a.melnichuk@yahoo.com>
2019-06-04 22:10:50 +0300

    Add lib path to WavesCore

---
+++ WavesCore/Examples/Pods/WavesCore/WavesCore/Sources/paytomat_waves_core/include/ptc_waves.h
@@ -0,0 +1,17 @@
+//
+//  ptc_waves.h
+//  WavesCore
+//
+//  Created by Alex Melnichuk on 5/6/19.
+//  Copyright © 2019 Alex Melnichuk. All rights reserved.
+//
+
+#ifndef PTWAVES_H
+#define PTWAVES_H
+
+#include "ptc_waves_test.h"
+#include "ptc_waves_address.h"
+#include "ptc_waves_crypto.h"
+#include "ptc_waves_transaction.h"
+
+#endif
