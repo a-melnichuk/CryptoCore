8300b813d6ae917102c3d21097d3090eda2c245a
Alex Melnichuk <a.melnichuk@yahoo.com>
2019-06-05 00:14:26 +0300

    Fix callCrypto

--- CryptoCore/Sources/Crypto.swift
+++ CryptoCore/Sources/Crypto.swift
@@ -93,11 +93,12 @@ extension Crypto {
             fatalError("\(#function) outBytes cannot be negative")
         }
         var out = Data(count: outCount)
+        let count = data.count
         let result: ptc_result = out.withUnsafeMutableBytes { outBuf in
             data.withUnsafeBytes { dataBuf in
                 if let dataPtr = dataBuf.baseAddress,
                     let outPtr = outBuf.bindMemory(to: UInt8.self).baseAddress {
-                    return callback(dataPtr, outCount, outPtr)
+                    return callback(dataPtr, count, outPtr)
                 }
                 return PTC_ERROR_GENERAL
             }
