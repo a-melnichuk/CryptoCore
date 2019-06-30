ced7b833a74d7725cd1d522f1e138872815f0de6
Alex Melnichuk <a.melnichuk@yahoo.com>
2019-06-30 20:45:28 +0300

    Add Crypto.randomBytes() functionality

--- CryptoCore/Sources/Crypto+Util.swift
+++ CryptoCore/Sources/Crypto+Util.swift
@@ -10,6 +10,17 @@ import Foundation
 import paytomat_crypto_core
 
 public extension Crypto {
+    static func randomBytes(_ count: Int) -> Data? {
+        var randomBytes = Data(count: count)
+        let result: ptc_result = randomBytes.withUnsafeMutableBytes { buf in
+            if let ptr = buf.bindMemory(to: UInt8.self).baseAddress {
+                return ptc_random_bytes(ptr, buf.count)
+            }
+            return PTC_ERROR_GENERAL
+        }
+        return result == PTC_SUCCESS ? randomBytes : nil
+    }
+    
     static func data(fromHex hex: String) -> Data? {
         guard !hex.isEmpty else {
             return Data()
