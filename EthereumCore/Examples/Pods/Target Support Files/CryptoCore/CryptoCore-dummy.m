a806acd954a78ed632dbd75c6813d99cdc87b4b0
Alex Melnichuk <a.melnichuk@yahoo.com>
2019-07-15 12:45:21 +0300

    Update git cache for gitignores

--- EthereumCore/Examples/Pods/Target Support Files/CryptoCore/CryptoCore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CryptoCore : NSObject
-@end
-@implementation PodsDummy_CryptoCore
-@end
--- NEMCore/Examples/Pods/Target Support Files/NEMCore/NEMCore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NEMCore : NSObject
-@end
-@implementation PodsDummy_NEMCore
-@end
